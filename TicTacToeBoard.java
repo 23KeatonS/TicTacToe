847810af42ab85eebc51727c34484701d10a845a
Keaton Stwalley <23keatonstwalley@usd266.com>
2023-03-21 12:23:48 -0500

    added some methods not much

--- TicTacToeBoard.java
+++ TicTacToeBoard.java
@@ -1,6 +1,51 @@
 class TicTacToeBoard{
-    public static void main(String[] args) {
-        System.out.println("hi");
+    
+    private int[][] board;
+    private int gameOver;
+
+
+    public TicTacToeBoard(){
+        this.board = new int [][] {{0,0,0},{0,0,0},{0,0,0}};
+        this.gameOver = 0;
+        
+        
+
     }
+
+
+    public void makeMove(int r, int c,int player){
+        this.board[r][c] = player;
+    }
+
+    public boolean isValidMove(int r, int c){
+        return (this.board[r][c]==0);
+    }
+
+
+    public int[][] getBoard() {
+        return board;
+    }
+    public int getGameOver() {
+        return gameOver;
+    }
+
+
+
+
+
+
+    public String toString(){
+        String retVal = "";
+        
+
+        return retVal;
+    }
+
+
+    
+    
+    
+    
+    
 }
 
