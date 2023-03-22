87c8da5104445253f8c20b2ae9005badcdfcb68a
tammy vu <tammy.vsk@gmail.com>
2023-03-22 12:13:46 -0500

    optimizing promptstart

--- TicTacToeConsole.java
+++ TicTacToeConsole.java
@@ -1,38 +1,44 @@
-import java.util.Scanner; 
+import java.util.Scanner;
 
 class TicTacToeConsole{
     private static Scanner usr = new Scanner(System.in); 
-    
-    public static void main(String args[]){
-        
+
+    private static void promptStart() {
         System.out.println("Welcome to TicTacToe \n Begin? y/n");
-        String begin = usr.nextLine();
-        begin = begin.toLowerCase();
-        if (begin.equals("y")){
-            System.out.println(" Beginning...");
-        }else if (begin.equals("n")){
+
+        String begin = usr.nextLine().toLowerCase();
+        if (begin.equals("n")){
             System.out.println("Player is not ready to play. \n Closing.");
             System.exit(0);     
         }else{
-            while (!(begin.equals("y") && !(begin.equals("n")))){
+            while (!begin.equals("y") && !begin.equals("n")){
                 System.out.println("Invalid input. Try again.");
-                begin = usr.nextLine();
-                begin = begin.toLowerCase();
-                if (begin.equals("y")){
-                    System.out.println(" Beginning...");
-                }else if (begin.equals("n")){
+                begin = usr.nextLine().toLowerCase();
+                if (begin.equals("n")) {
                     System.out.println("Player is not ready to play. \n Closing.");
                     System.exit(0);    
-                } 
-
-            }
-            
+                }
+            } 
         }
+        System.out.println(" Beginning...");
+    }
+
+    private static void p1InvalidInput(String p1move){
+        if (p1move.length() != 2){
+            System.out.println(" Invalid input. Try again.");
+        }
+    }
+    
+    public static void main(String args[]){
+
+        promptStart();
+
         TicTacToeBoard board = new TicTacToeBoard();
         System.out.println(board);
 
         System.out.println("You are player 1. Make your move.");
         String p1move = usr.nextLine();
+        p1InvalidInput(p1move);
         
 
     }
