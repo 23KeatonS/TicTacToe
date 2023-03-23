5805fb3b51e251f1ebcab84c7baf6afaf89245bf
Keaton Stwalley <23keatonstwalley@usd266.com>
2023-03-23 12:11:19 -0500

    added winConditions

--- TicTacToeBoard.java
+++ TicTacToeBoard.java
@@ -2,11 +2,13 @@ class TicTacToeBoard{
     
     private int[][] board;
     private int status;
+    public static int turnCount = 0;
 
 
     public TicTacToeBoard(){
         this.board = new int [][] {{0,0,0},{0,0,0},{0,0,0}};
         this.status = 0;
+        
         
         
 
@@ -15,6 +17,7 @@ class TicTacToeBoard{
 
     public void makeMove(int r, int c,int player){
         this.board[r][c] = player;
+        turnCount++;
     }
 
     public boolean isValidMove(int r, int c){
@@ -22,12 +25,79 @@ class TicTacToeBoard{
     }
 
 
+
+    public boolean threeInARow(int player, int startC,int startR, int dx, int dy){
+        for(int i =0;i<3;i++){
+            if(this.board[startR][startC]!=player){
+                return false;
+            }
+            startR+=dy;
+            startC+=dx;
+        }
+
+        return true;
+    }
+
+
+
+
+
+    private boolean playerWon(int player){
+        if(threeInARow(player,0,0,1,0)){
+            return true;
+        }
+        if(threeInARow(player, 0, 0, 0, 1)){
+            return true;
+        }
+        if(threeInARow(player, 0, 0, 1, 1)){
+            return true;
+        }
+        if(threeInARow(player,1,0,0,1)){
+            return true;
+        }
+        if(threeInARow(player, 2, 0, 0, 1)){
+            return true;
+        }
+        if(threeInARow(player, 0, 1, 1, 0)){
+            return true;
+        }
+        if(threeInARow(player, 0, 2, 1, 0)){
+            return true;
+        }
+        if(threeInARow(player, 2, 0, -1, 0)){
+            return true;
+        }
+        
+
+
+
+        return false;
+    }
+
+
+
+
+
+
     public int getCellContents(int r, int c) {
         return board[r][c];
     }
     public int getStatus() {
-        return status;
+       if(playerWon(1)){
+        return 1;
+       } else if(playerWon(2)){
+        return 2;
+       }
+       if(turnCount>=9){
+        return -1;
+       }else{
+        return 0;
+       }
+    
+    
+       
     }
+
 
 
 
@@ -62,8 +132,17 @@ class TicTacToeBoard{
 
 
     public static void main(String[] args) {
+        /* 
         TicTacToeBoard board = new TicTacToeBoard();
         System.out.println(board);
+        board.makeMove(0, 0, 1);
+        System.out.println(board.getStatus());
+        board.makeMove(0, 1, 1);
+        System.out.println(board.getStatus());
+        board.makeMove(0, 2, 1);
+        System.out.println(board.getStatus());
+        */
+
     }
     
     
